8d80f4162c8276fdda9f9b6a946c0351096bbb1b
Eric Lowe <ericlowe25@gmail.ocm>
2022-02-02 20:51:49 -0500

    changed font size on navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,77 @@
 // TODO: Declare any global variables we need
+let headsRolls = 0
+let tailsRolls = 0
 
-
+// TODO: Add event listener and handler for flip and clear buttons
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+   
+    // Flip Button Click Handler  
+    document.getElementById('flip').addEventListener('click', () => {
+    
+    // TODO: Determine flip outcome    
+        let flippedHeads = Math.random() < 0.5
+        if (flippedHeads) {
+             document.getElementById('penny-image').src = 'assets/images/penny-heads.jpg'
+             document.getElementById('message').textContent = 'You Flipped Heads!'
+             headsRolls += 1
+        }
+         else {
+             document.getElementById('penny-image').src = 'assets/images/penny-tails.jpg'
+             document.getElementById('message').textContent = 'You Flipped tails!'
+             tailsRolls += 1
+         }
+         
+    // TODO: Calculate the total number of rolls/flips
+             let total = headsRolls + tailsRolls
 
-    // TODO: Add event listener and handler for flip and clear buttons
+    // Make variables to track the percentages of heads and tails    
+         let percentHeads = 0
+         let percentTails = 0
+         if (total > 0) {
+     
+        // TODO: Use the calculated total to calculate the percentages  
+         percentHeads = Math.round((headsRolls / total) * 100)
+         percentTails = Math.round((tailsRolls / total) * 100)
+         }            
 
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+// TODO: Update the display of each table cell
+         document.getElementById('heads').textContent = headsRolls
+         document.getElementById('heads-percent').textContent = percentHeads + '%'
+         document.getElementById('tails').textContent = tailsRolls
+         document.getElementById('tails-percent').textContent = percentTails + '%'
 })
+// TODO: Reset global variables to 0
+document.getElementById('clear').addEventListener('click', function () {
+     headsRolls = 0
+     tailsRolls = 0
+
+     document.getElementById('message').textContent = 'Let\'s Get Rolling!'
+
+     let total = headsRolls + tailsRolls
+
+    // Create variables to track percent heads and tails
+     let percentHeads = 0
+     let percentTails = 0
+     if (total > 0) {
+        percentHeads = Math.round((headsRolls / total) * 100)
+        percentTails = Math.round((tailsRolls / total) * 100)
+     }
+
+     document.getElementById('heads').textContent = headsRolls
+     document.getElementById('heads-percent').textContent = percentHeads + '%'
+     document.getElementById('tails').textContent = tailsRolls
+     document.getElementById('tails-percent').textContent = percentTails + '%'
+     })
+ })
+        
+        
+
+        
+
+ 
+
+
+        // Uses the Math.random function to generate a random number.
+        // If the rand num is less than 1/2, it is classified as heads.
+        // Otherwise, the num is above 1/2 and is classified as tails.
+         // Calls the main function to begin the coin flipping simulation.
